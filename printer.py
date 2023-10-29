a295caa81f787d8862de858641350a16b9bfd72b
suk-6 <me@suk.kr>
2023-10-29 15:07:19 +0900

    Fix: Printer Setting

--- printer.py
+++ printer.py
@@ -4,16 +4,15 @@ import os
 
 def printer(file):
     printerName = win32print.GetDefaultPrinter()
-    printerInfo = win32print.GetPrinter(printerName, 2)
 
     if not os.path.isfile(file):
         return False
     else:
-        # 프린트 작업 설정
+        hprinter = win32print.OpenPrinter(printerName)
+        printerInfo = win32print.GetPrinter(hprinter, 2)
         printerInfo['pPortName'] = printerName
         printerInfo['pDatatype'] = 'RAW'
         
-        hprinter = win32print.OpenPrinter(printerName)
         win32print.StartDocPrinter(hprinter, 1, ('Print Job', None, 'RAW'))
         win32print.StartPagePrinter(hprinter)
         with open(file, 'rb') as f:
